45364ac46eaed92e7171ce80e72ae103f592f7a4
mattisafur <46404761+mattisafur@users.noreply.github.com>
2025-10-06 15:55:20 +0200

    defined some util commands
    most commands are not implemented yet

--- util.py
+++ util.py
@@ -1,2 +1,18 @@
 def get_user_input() -> list[str]:
     return input("> ").strip().split(" ")
+
+
+def print_leaderboard() -> None:
+    raise NotImplementedError
+
+
+def print_stats() -> None:
+    raise NotImplementedError
+
+
+def quit_game() -> None:
+    exit()
+
+
+def pause_game() -> None:
+    raise NotImplementedError
