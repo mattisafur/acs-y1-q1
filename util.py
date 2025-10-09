c49890297f0e5ed1c5dee26c784cd2f0c542c87a
mattisafur <46404761+mattisafur@users.noreply.github.com>
2025-10-09 15:54:20 +0200

    implemented util functions

--- util.py
+++ util.py
@@ -8,14 +8,6 @@ def get_user_input() -> list[str]:
     return input("> ").strip().split(" ")
 
 
-def display_leaderboard() -> None:
-    raise NotImplementedError
-
-
-def display_stats() -> None:
-    raise NotImplementedError
-
-
 def quit_game() -> None:
     exit()
 
@@ -24,8 +16,39 @@ def pause_game(state: State) -> None:
     raise NotImplementedError
 
 
-def display_inventory(state: State) -> None:
+def display_leaderboard() -> None:
     raise NotImplementedError
+
+
+def display_stats(state: State) -> None:
+    raise NotImplementedError
+
+
+def display_inventory(state: State) -> None:
+    print("Inventory:\n" + "\n".join(state.inventory))
+
+
+def update_time_played(
+    current_time_played: TimeDelta, session_start_time: DateTime
+) -> TimeDelta:
+    time_played_this_session = DateTime.now() - session_start_time
+    return current_time_played + time_played_this_session
+
+
+def display_go_list(rooms: list[str]) -> None:
+    print("Connected rooms:\n", "\n".join(rooms))
+
+
+def display_take_list(items: list[str]) -> None:
+    print("Items available to pick up:\n" + "\n".join(items))
+
+
+def display_invalid_command() -> None:
+    print("Invalid command.")
+
+
+def display_invalid_syntax(command_name: str) -> None:
+    print(f"Invalid syntax\nUser `{command_name} ?` for help")
 
 
 def display_go_help() -> None:
@@ -39,24 +62,27 @@ def display_go_help() -> None:
     )
 
 
-def display_invalid_syntax(command_name: str) -> None:
-    print(f"Invalid syntax\nUser `{command_name} ?` for help")
-
-
-def display_go_list(rooms: list[str]) -> None:
-    print("Connected rooms:\n", "\n".join(rooms))
+def display_look_help() -> None:
+    print("look\nlooks around the room and says what you see")
 
 
 def display_take_help() -> None:
-    raise NotImplementedError
+    print(
+        "take <item>|list\n"
+        "Pick up an item from the room and place it in your inventory\n"
+        "<item>\n"
+        "\tpick up the specified item\n"
+        "list\n"
+        "\tlist items possible to take"
+    )
 
 
-def display_take_list(items: list[str]) -> None:
-    raise NotImplementedError
+def display_answer_help() -> None:
+    print("answer <answer>\nAnswer a challenge")
 
 
-def display_invalid_command() -> None:
-    print("Invalid command.")
+def display_inventory_help() -> None:
+    print("inventory\nDisplay the items currently in your inventory")
 
 
 def display_new_help() -> None:
@@ -69,10 +95,3 @@ def display_load_help() -> None:
 
 def display_delete_help() -> None:
     raise NotImplementedError
-
-
-def update_time_played(
-    current_time_played: TimeDelta, session_start_time: DateTime
-) -> TimeDelta:
-    time_played_this_session = DateTime.now() - session_start_time
-    return current_time_played + time_played_this_session
